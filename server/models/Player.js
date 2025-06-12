8ea1eef079292186670e41d6e0b1e594753032a4
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-06-12 11:39:52 +0530

    Update Player.js

--- server/models/Player.js
+++ server/models/Player.js
@@ -1,11 +1,12 @@
 // server/models/Player.js
+
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
-  name: String,
-  basePrice: Number,
-  soldTo: String, // team name
-  soldPrice: Number
+  name: { type: String, required: true },
+  basePrice: { type: Number, required: true },
+  team: { type: String, default: null },
+  soldPrice: { type: Number, default: null },
 });
 
-module.exports = mongoose.model("Player", playerSchema);
+module.exports = mongoose.model('Player', playerSchema);
