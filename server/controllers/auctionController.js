4cb755754bf33149f6e937a3a2d59f07bea3a71d
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-06-12 12:39:31 +0530

    Update auctionController.js

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -1,13 +1,11 @@
-// server/controllers/auctionController.js
-const Player = require("../models/Player");
 const Team = require("../models/Team");
 
-exports.getTeams = async (req, res) => {
-  const teams = await Team.find();
-  res.json(teams);
-};
-
-exports.getPlayers = async (req, res) => {
-  const players = await Player.find();
-  res.json(players);
+exports.createTeams = async (req, res) => {
+  try {
+    await Team.deleteMany(); // optional: clear previous teams
+    const teams = await Team.insertMany(req.body);
+    res.status(201).json(teams);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to save teams" });
+  }
 };
