e928d5e5f5749a110c5921637797a903bf334cf4
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-06-12 13:04:50 +0530

    Update socketHandler.js

--- server/socket/socketHandler.js
+++ server/socket/socketHandler.js
@@ -1,45 +1,75 @@
-// server/socket/socketHandler.js
-const Player = require('../models/Player');
-const Team = require('../models/Team');
+const Player = require("../models/Player");
+const Team = require("../models/Team");
 
 let currentPlayerIndex = 0;
 let players = [];
+let currentBid = 0;
+let lastBidder = null;
+let timer = null;
 
 module.exports = (io) => {
-  io.on('connection', (socket) => {
-    console.log("🟢 A user connected");
+  io.on("connection", (socket) => {
+    console.log("User connected");
 
     socket.on("start-auction", async () => {
       players = await Player.find();
-      io.emit("auction-started", players[currentPlayerIndex]);
+      currentPlayerIndex = 0;
+      startNextPlayer(io);
     });
 
     socket.on("next-player", () => {
-      currentPlayerIndex++;
-      if (currentPlayerIndex < players.length) {
-        io.emit("auction-next", players[currentPlayerIndex]);
-      } else {
-        io.emit("auction-finished");
-      }
+      startNextPlayer(io);
     });
 
     socket.on("place-bid", async ({ teamName, amount }) => {
-      const team = await Team.findOne({ name: teamName });
+      if (!players[currentPlayerIndex]) return;
+
       const player = players[currentPlayerIndex];
+      const team = await Team.findOne({ name: teamName });
 
-      if (team.budget >= amount) {
-        team.budget -= amount;
-        team.playersWon.push({ name: player.name, price: amount });
-        await team.save();
+      if (!team || team.budget < amount / 100) return;
 
-        player.soldTo = teamName;
-        player.soldPrice = amount;
-        await player.save();
+      currentBid = amount;
+      lastBidder = teamName;
 
-        io.emit("player-sold", { player, team });
-      } else {
-        socket.emit("error", "Not enough budget");
-      }
+      // Notify all clients of current updated bid
+      io.emit("bid-updated", { playerName: player.name, amount, teamName });
+
+      // Restart timer
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        sellPlayer(io, player, team);
+      }, 3000); // 3 seconds timeout
     });
   });
 };
+
+async function startNextPlayer(io) {
+  if (timer) clearTimeout(timer);
+  if (currentPlayerIndex >= players.length) {
+    io.emit("auction-complete");
+    return;
+  }
+
+  currentBid = 0;
+  lastBidder = null;
+
+  const player = players[currentPlayerIndex];
+  io.emit("auction-started", player);
+}
+
+async function sellPlayer(io, player, team) {
+  const bidPrice = currentBid / 100;
+
+  player.soldPrice = bidPrice;
+  player.soldTo = team.name;
+  await player.save();
+
+  team.budget -= bidPrice;
+  await team.save();
+
+  io.emit("player-sold", { player, team });
+
+  currentPlayerIndex++;
+  setTimeout(() => startNextPlayer(io), 2000); // short delay before next player
+}
